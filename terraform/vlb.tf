7e1fa32d8e9323270d09a5a3c57c244730d5f722
Honahuku <tech@honahuku.com>
2023-06-14 03:52:13 +0900

    VLBのimport

---
+++ terraform/vlb.tf
@@ -0,0 +1,19 @@
+resource "vultr_load_balancer" "asinoyu-lb" {
+  region              = "itm"
+  label               = "a4511a7a7c2b7476eb6a2c83c9fbc08d"
+  balancing_algorithm = "roundrobin"
+
+  forwarding_rules {
+    frontend_protocol = "tcp"
+    frontend_port = 80
+    backend_protocol = "tcp"
+    backend_port = 32277
+  }
+
+  forwarding_rules {
+    frontend_protocol = "tcp"
+    frontend_port = 443
+    backend_protocol = "tcp"
+    backend_port = 31603
+  }
+}
