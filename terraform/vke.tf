dd9c85b872450cacc3b3d659b29ebe3b082eaeea
Honahuku <tech@honahuku.com>
2023-06-14 00:28:16 +0900

    node_poolの指定を変更

--- terraform/vke.tf
+++ terraform/vke.tf
@@ -2,15 +2,13 @@ resource "vultr_kubernetes" "asinoyu" {
   region  = "itm" # 大阪リージョン
   label   = "asinoyu"
   version = "v1.25"
-}
 
-resource "vultr_kubernetes_node_pools" "general" {
-  cluster_id    = vultr_kubernetes.asinoyu.id
-  node_quantity = 3
-  plan          = "vc2-1c-1gb"
-  label         = "general"
-  tag           = "general"
-  auto_scaler   = true
-  min_nodes     = 1
-  max_nodes     = 3
+  node_pools {
+    node_quantity = 2
+    plan          = "vc2-1c-2gb"
+    label         = "general"
+    auto_scaler   = true
+    min_nodes     = 1
+    max_nodes     = 2
+  }
 }
