b16d5e4e38379871d2d84e780ee8d9711b23165a
Honahuku <tech@honahuku.com>
2023-07-10 12:07:14 +0900

    fix: instance07を追加

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,35 +3,35 @@
 # OSの一覧を抽出
 # curl "https://api.vultr.com/v2/os" -X GET | jq > os.json
 
-resource "vultr_instance" "k8s_test04" {
+resource "vultr_instance" "k8s_test07" {
     plan = "vc2-1c-2gb"
     region = "itm"
     os_id = 1743
-    label = "k8s_test04"
+    label = "k8s_test07"
     enable_ipv6 = true
     backups = "disabled"
     ddos_protection = false
     ssh_key_ids = ["${vultr_ssh_key.github.id}"]
 }
 
-resource "vultr_instance" "k8s_test05" {
-    plan = "vc2-1c-2gb"
-    region = "itm"
-    os_id = 1743
-    label = "k8s-test05"
-    enable_ipv6 = true
-    backups = "disabled"
-    ddos_protection = false
-    ssh_key_ids = ["${vultr_ssh_key.github.id}"]
-}
+# resource "vultr_instance" "k8s_test05" {
+#     plan = "vc2-1c-2gb"
+#     region = "itm"
+#     os_id = 1743
+#     label = "k8s-test05"
+#     enable_ipv6 = true
+#     backups = "disabled"
+#     ddos_protection = false
+#     ssh_key_ids = ["${vultr_ssh_key.github.id}"]
+# }
 
-resource "vultr_instance" "k8s_test06" {
-    plan = "vc2-1c-2gb"
-    region = "itm"
-    os_id = 1743
-    label = "k8s-test06"
-    enable_ipv6 = true
-    backups = "disabled"
-    ddos_protection = false
-    ssh_key_ids = ["${vultr_ssh_key.github.id}"]
-}
+# resource "vultr_instance" "k8s_test06" {
+#     plan = "vc2-1c-2gb"
+#     region = "itm"
+#     os_id = 1743
+#     label = "k8s-test06"
+#     enable_ipv6 = true
+#     backups = "disabled"
+#     ddos_protection = false
+#     ssh_key_ids = ["${vultr_ssh_key.github.id}"]
+# }
