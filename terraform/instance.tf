a17b6fe9ddce8a766d2c872348678e4fa2f47bcf
Honahuku <tech@honahuku.com>
2023-07-10 10:04:48 +0900

    feat: k8s instanceを立て直す

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,33 +3,33 @@
 # OSの一覧を抽出
 # curl "https://api.vultr.com/v2/os" -X GET | jq > os.json
 
-resource "vultr_instance" "k8s_test01" {
+resource "vultr_instance" "k8s_test04" {
     plan = "vc2-1c-2gb"
     region = "itm"
     os_id = 1743
-    label = "k8s-test01"
+    label = "k8s_test04"
     enable_ipv6 = true
     backups = "disabled"
     ddos_protection = false
     ssh_key_ids = ["${vultr_ssh_key.github.id}"]
 }
 
-resource "vultr_instance" "k8s_test02" {
+resource "vultr_instance" "k8s_test05" {
     plan = "vc2-1c-2gb"
     region = "itm"
     os_id = 1743
-    label = "k8s-test02"
+    label = "k8s-test05"
     enable_ipv6 = true
     backups = "disabled"
     ddos_protection = false
     ssh_key_ids = ["${vultr_ssh_key.github.id}"]
 }
 
-resource "vultr_instance" "k8s_test03" {
+resource "vultr_instance" "k8s_test06" {
     plan = "vc2-1c-2gb"
     region = "itm"
     os_id = 1743
-    label = "k8s-test03"
+    label = "k8s-test06"
     enable_ipv6 = true
     backups = "disabled"
     ddos_protection = false
